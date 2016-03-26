f789875161fbffeb43b8c9d0e070b0f2e8de4eff
celestian <petr.celestian@gmail.com>
2016-03-26 08:37:32 +0100

    Environment

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8-*-
+
+from distutils.core import setup
+
+setup(
+    name='chargen',
+    version='0.0.1',
+    description='General (RPG) Character Generator',
+    author="celestian",
+    license='MIT',
+
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
+)
